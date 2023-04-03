a04f1b17e6314e0def5eb8019cc99cdc314f2d89
Jon Zubizarreta <jonzubi97@gmail.com>
2023-04-03 15:36:46 +0200

    Traducciones funcional

--- src/router.tsx
+++ src/router.tsx
@@ -3,6 +3,21 @@ import { createStackNavigator } from '@react-navigation/stack';
 import SplashScreen from './screens/SplashScreen/SplashScreen';
 import LoginScreen from './screens/LoginScreen/LoginScreen';
 import { StackParamList } from './types/StackParamList';
+import i18n from 'i18next';
+import { initReactI18next } from 'react-i18next';
+import eus_json from './i18n/eus.json';
+
+i18n.use(initReactI18next).init({
+  resources: {
+    eus: {
+      translation: eus_json,
+    },
+  },
+  fallbackLng: 'eus',
+  interpolation: {
+    escapeValue: false,
+  },
+});
 
 const Stack = createStackNavigator<StackParamList>();
 
--- src/screens/LoginScreen/LoginScreen.tsx
+++ src/screens/LoginScreen/LoginScreen.tsx
@@ -3,27 +3,30 @@ import styles from './LoginScreen.android.styles';
 import Logo from '../../components/Logo/Logo';
 import { Input, Button } from '@rneui/themed';
 import { AntDesign } from '@expo/vector-icons';
+import { useTranslation } from 'react-i18next';
 
 export default function LoginScreen() {
+  const { t } = useTranslation();
+
   return (
     <View style={styles.container}>
       <Logo style={styles.logo} />
 
       <Input
-        placeholder="Sartu zure emaila..."
+        placeholder={t('forms.enter_email_placeholder')}
         leftIcon={
           <AntDesign name={'mail'} size={24} style={{ marginRight: 5 }} />
         }
       />
       <Input
-        placeholder="Sartu zure pasahitza..."
+        placeholder={t('forms.enter_pass_placeholder')}
         secureTextEntry={true}
         leftIcon={
           <AntDesign name={'key'} size={24} style={{ marginRight: 5 }} />
         }
       />
       <Button
-        title={'Sartu'}
+        title={t('forms.login_button')}
         color={'#00BB69'}
         containerStyle={{
           width: '100%',
