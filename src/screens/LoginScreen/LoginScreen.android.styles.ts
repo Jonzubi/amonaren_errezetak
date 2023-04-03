12761b4444f777673fa0f32f660a70e3dafe96d0
Jon Zubizarreta <jonzubi97@gmail.com>
2023-04-03 13:00:55 +0200

    LoginScreen utiliza logo pero mas pequeño

--- src/screens/LoginScreen/LoginScreen.android.styles.ts
+++ src/screens/LoginScreen/LoginScreen.android.styles.ts
@@ -7,4 +7,8 @@ export default StyleSheet.create({
     alignItems: 'center',
     backgroundColor: 'white',
   },
+  logo: {
+    height: 300,
+    width: 300,
+  },
 });
--- src/screens/LoginScreen/LoginScreen.tsx
+++ src/screens/LoginScreen/LoginScreen.tsx
@@ -1,10 +1,11 @@
 import { View, Text } from 'react-native';
 import styles from './LoginScreen.android.styles';
+import Logo from '../../components/Logo/Logo';
 
 export default function LoginScreen() {
   return (
     <View style={styles.container}>
-      <Text>Login</Text>
+      <Logo style={styles.logo} />
     </View>
   );
 }
