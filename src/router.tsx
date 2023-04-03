934beb3a20955c051125f8ee8b9270312e433197
Jon Zubizarreta <jonzubi97@gmail.com>
2023-04-03 10:56:52 +0200

    Creo login y lo pongo en la navegacion

--- src/router.tsx
+++ src/router.tsx
@@ -1,6 +1,7 @@
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import SplashScreen from './screens/SplashScreen/SplashScreen';
+import LoginScreen from './screens/LoginScreen/LoginScreen';
 
 const Stack = createNativeStackNavigator();
 
@@ -11,6 +12,13 @@ export default function Router() {
         <Stack.Screen
           name="Splash"
           component={SplashScreen}
+          options={{
+            headerShown: false,
+          }}
+        />
+        <Stack.Screen
+          name="Login"
+          component={LoginScreen}
           options={{
             headerShown: false,
           }}
--- src/screens/LoginScreen/LoginScreen.tsx
+++ src/screens/LoginScreen/LoginScreen.tsx
@@ -1,1 +1,10 @@
-export default function LoginScreen() {}
+import { View, Text } from 'react-native';
+import styles from './LoginScreen.android.styles';
+
+export default function LoginScreen() {
+  return (
+    <View style={styles.container}>
+      <Text>Login</Text>
+    </View>
+  );
+}
