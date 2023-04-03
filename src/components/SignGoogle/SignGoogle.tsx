a265218bb916ae6c9bc4e81b4361f98ede52f184
Jon Zubizarreta <jonzubi97@gmail.com>
2023-04-03 22:07:02 +0200

    Exporto client ids de Google

--- src/components/SignGoogle/SignGoogle.tsx
+++ src/components/SignGoogle/SignGoogle.tsx
@@ -5,6 +5,11 @@ import { useTranslation } from 'react-i18next';
 import { useEffect, useState } from 'react';
 import { View } from 'react-native';
 import { AntDesign } from '@expo/vector-icons';
+import {
+  GOOGLE_ANDROID_CLIENT_ID,
+  GOOGLE_EXPO_CLIENT_ID,
+  GOOGLE_WEB_CLIENT_ID,
+} from '../../constants/constants';
 
 WebBrowser.maybeCompleteAuthSession();
 
@@ -15,12 +20,9 @@ export default function SignGoogle() {
   const [userInfo, setUserInfo] = useState(null);
 
   const [request, response, promptAsync] = Google.useAuthRequest({
-    androidClientId:
-      '430573762472-rm2c2krf6nih6ae57gnt8mqftv25bbvj.apps.googleusercontent.com',
-    webClientId:
-      '430573762472-qv7ar6kjfik8b44gkhfcqdmvchgc7ro3.apps.googleusercontent.com',
-    expoClientId:
-      '430573762472-rm2c2krf6nih6ae57gnt8mqftv25bbvj.apps.googleusercontent.com',
+    androidClientId: GOOGLE_ANDROID_CLIENT_ID,
+    webClientId: GOOGLE_WEB_CLIENT_ID,
+    expoClientId: GOOGLE_EXPO_CLIENT_ID,
   });
 
   useEffect(() => {
---
+++ src/constants/constants.ts
@@ -0,0 +1,6 @@
+export const GOOGLE_WEB_CLIENT_ID =
+  '430573762472-qv7ar6kjfik8b44gkhfcqdmvchgc7ro3.apps.googleusercontent.com';
+export const GOOGLE_ANDROID_CLIENT_ID =
+  '430573762472-rm2c2krf6nih6ae57gnt8mqftv25bbvj.apps.googleusercontent.com';
+export const GOOGLE_EXPO_CLIENT_ID =
+  '430573762472-rm2c2krf6nih6ae57gnt8mqftv25bbvj.apps.googleusercontent.com';
