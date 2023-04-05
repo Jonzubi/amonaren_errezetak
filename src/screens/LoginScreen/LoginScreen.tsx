e00043ef93f20e5358998681c26292569d0ec418
Jon Zubizarreta <jonzubi97@gmail.com>
2023-04-05 13:49:17 +0200

    Login recibo access token

--- src/screens/LoginScreen/LoginScreen.tsx
+++ src/screens/LoginScreen/LoginScreen.tsx
@@ -1,4 +1,4 @@
-import { View, Text } from 'react-native';
+import { View, Text, ActivityIndicator } from 'react-native';
 import styles from './LoginScreen.android.styles';
 import Logo from '../../components/Logo/Logo';
 import { Input, Button, Divider } from '@rneui/themed';
@@ -6,9 +6,30 @@ import { AntDesign } from '@expo/vector-icons';
 import { useTranslation } from 'react-i18next';
 import SignGoogle from '../../components/SignGoogle/SignGoogle';
 import colors from '../../constants/colors';
+import { useRef, useState } from 'react';
+import { isEmail } from '../../utils/functions';
+import { login } from '../../api/user/user';
 
 export default function LoginScreen({ navigation }) {
   const { t } = useTranslation();
+
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [errorEmail, setErrorEmail] = useState('');
+  const [isLoading, setIsLoading] = useState(false);
+  const emailRef = useRef();
+
+  const validateForm = (): boolean => {
+    let isValid = true;
+    setErrorEmail('');
+
+    if (!isEmail(email)) {
+      emailRef.current.shake();
+      setErrorEmail(t('registerScreen.not_email'));
+      isValid = false;
+    }
+    return isValid;
+  };
 
   const onSignUp = () => {
     navigation.navigate('SignUp');
@@ -18,6 +39,15 @@ export default function LoginScreen({ navigation }) {
     console.log(userData);
   };
 
+  const handleLogin = async () => {
+    if (!validateForm()) return;
+    setIsLoading(true);
+    const tokenData = await login({ email, password }).catch((error) =>
+      console.log(error),
+    );
+    setIsLoading(false);
+    console.log(tokenData);
+  };
   return (
     <View style={styles.container}>
       <Logo style={styles.logo} />
@@ -27,6 +57,9 @@ export default function LoginScreen({ navigation }) {
         leftIcon={
           <AntDesign name={'mail'} size={24} style={{ marginRight: 5 }} />
         }
+        onChangeText={(value) => setEmail(value)}
+        errorMessage={errorEmail}
+        ref={emailRef}
       />
       <Input
         placeholder={t('forms.enter_pass_placeholder')}
@@ -34,12 +67,23 @@ export default function LoginScreen({ navigation }) {
         leftIcon={
           <AntDesign name={'key'} size={24} style={{ marginRight: 5 }} />
         }
+        onChangeText={(value) => setPassword(value)}
       />
-      <Button
-        title={t('forms.login_button')}
-        color={colors.MAIN_GREEN}
-        containerStyle={styles.button}
-      />
+      {isLoading && (
+        <ActivityIndicator
+          size={'large'}
+          color={colors.MAIN_GREEN}
+          style={styles.button}
+        />
+      )}
+      {!isLoading && (
+        <Button
+          title={t('forms.login_button')}
+          color={colors.MAIN_GREEN}
+          containerStyle={styles.button}
+          onPress={handleLogin}
+        />
+      )}
       <Divider style={styles.divider} />
       <SignGoogle onUserData={handleOnUserData} />
       <View style={styles.registerView}>
