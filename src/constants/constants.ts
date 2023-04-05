9330c59d696a9648cd4979f363d1c86142cf9e77
Jon Zubizarreta <jonzubi97@gmail.com>
2023-04-05 12:14:12 +0200

    Arreglo api_url

--- src/constants/constants.ts
+++ src/constants/constants.ts
@@ -5,7 +5,7 @@ export const GOOGLE_ANDROID_CLIENT_ID =
 export const GOOGLE_EXPO_CLIENT_ID =
   '430573762472-rm2c2krf6nih6ae57gnt8mqftv25bbvj.apps.googleusercontent.com';
 
-const API_URL_DEV = 'http://localhost:3000/';
-const API_URL_PROD = 'http://localhost:3000/';
+const API_URL_DEV = 'http://localhost:3000';
+const API_URL_PROD = 'http://localhost:3000';
 export const API_URL =
   process.env.NODE_ENV === 'development' ? API_URL_DEV : API_URL_PROD;
