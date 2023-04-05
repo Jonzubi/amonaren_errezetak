054ee00ace036be5015f5078a469cb531e3feaa1
Jon Zubizarreta <jonzubi97@gmail.com>
2023-04-05 11:15:40 +0200

    Constante para api_url

--- src/constants/constants.ts
+++ src/constants/constants.ts
@@ -4,3 +4,8 @@ export const GOOGLE_ANDROID_CLIENT_ID =
   '430573762472-rm2c2krf6nih6ae57gnt8mqftv25bbvj.apps.googleusercontent.com';
 export const GOOGLE_EXPO_CLIENT_ID =
   '430573762472-rm2c2krf6nih6ae57gnt8mqftv25bbvj.apps.googleusercontent.com';
+
+const API_URL_DEV = 'http://localhost:3000/';
+const API_URL_PROD = 'http://localhost:3000/';
+export const API_URL =
+  process.env.NODE_ENV === 'development' ? API_URL_DEV : API_URL_PROD;
