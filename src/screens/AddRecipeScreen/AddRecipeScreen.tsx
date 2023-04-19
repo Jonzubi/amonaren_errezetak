0b1d96db1f522c9fc2e8a2b6bc7127a04ecdd927
Jon Zubizarreta <jonzubi97@gmail.com>
2023-04-19 22:19:36 +0200

    Añado titulo con traduccion en addRecipe

--- src/screens/AddRecipeScreen/AddRecipeScreen.tsx
+++ src/screens/AddRecipeScreen/AddRecipeScreen.tsx
@@ -1,11 +1,13 @@
 import { View } from 'react-native';
 import styles from './AddRecipeScreen.android.styles';
 import { Text } from '@rneui/themed';
+import { useTranslation } from 'react-i18next';
 
 export default function AddRecipeScreen() {
+  const { t } = useTranslation();
   return (
     <View style={styles.container}>
-      <Text h1>Añadir receta</Text>
+      <Text h3>{t('addRecipeScreen.title')}</Text>
     </View>
   );
 }
