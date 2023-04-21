087071fe25eaa31f61f753074095def015e305e6
Jon Zubizarreta <jonzubi97@gmail.com>
2023-04-21 15:18:34 +0200

    Migro a expo image picker

--- src/components/ChooseImages/ChooseImages.tsx
+++ src/components/ChooseImages/ChooseImages.tsx
@@ -2,27 +2,25 @@ import { View } from 'react-native';
 import { TouchableOpacity } from 'react-native';
 import styles from './ChooseImages.android.styles';
 import { Ionicons } from '@expo/vector-icons';
-import { useState } from 'react';
-import * as ImagePicker from 'react-native-image-picker';
-import { ImageLibraryOptions, Asset } from 'react-native-image-picker';
+import * as ImagePicker from 'expo-image-picker';
+import { ImagePickerOptions, MediaTypeOptions } from 'expo-image-picker';
 
 export interface ChooseImagesProps {
-  onImageChosen(image: Asset[]): void;
+  onImageChosen(image: ImagePicker.ImagePickerAsset[]): void;
 }
 
 export default function ChooseImages(props: ChooseImagesProps) {
   const { onImageChosen } = props;
-  const handleChooseImage = () => {
-    let options: ImageLibraryOptions = {
-      mediaType: 'photo',
-      selectionLimit: 5,
+  const handleChooseImage = async () => {
+    let options: ImagePickerOptions = {
+      mediaTypes: MediaTypeOptions.Images,
+      allowsMultipleSelection: true,
     };
-    ImagePicker.launchImageLibrary(options, (response) => {
-      if (response.didCancel || response.errorCode) return;
-      if (response.assets === undefined) return;
-      const source = { uri: response.assets };
-      onImageChosen(source.uri);
-    });
+    const response = await ImagePicker.launchImageLibraryAsync(options);
+    if (response.canceled) return;
+    if (response.assets === undefined) return;
+    const source = response.assets;
+    onImageChosen(source);
   };
   return (
     <View style={styles.container}>
