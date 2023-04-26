482362c72595f01ac84954b70dd35028a32e74d0
Jon Zubizarreta <jonzubi97@gmail.com>
2023-04-26 19:17:33 +0200

    Estio para sobreescribir el chooseImage

--- src/screens/AddRecipeScreen/AddRecipeScreen.android.styles.ts
+++ src/screens/AddRecipeScreen/AddRecipeScreen.android.styles.ts
@@ -1,8 +1,19 @@
 import { StyleSheet } from 'react-native';
+import colors from '../../constants/colors';
 export default StyleSheet.create({
   container: {
     flex: 1,
     alignItems: 'center',
     padding: 15,
+  },
+  addImage: {
+    width: '100%',
+    backgroundColor: colors.GREY,
+    height: 200,
+    justifyContent: 'center',
+    alignItems: 'center',
+    flexDirection: 'row',
+    padding: 15,
+    borderRadius: 20,
   },
 });
