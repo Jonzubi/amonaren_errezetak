04139c9bdd42d28d86bf32ca9b24cf4b973f2063
Jon Zubizarreta <jonzubi97@gmail.com>
2023-04-26 20:18:38 +0200

    Boton para añadir ingrediente

--- src/components/Ingredients/Ingredients.tsx
+++ src/components/Ingredients/Ingredients.tsx
@@ -1,12 +1,24 @@
 import { View, TouchableOpacity } from 'react-native';
 import { useState } from 'react';
 import { Text } from 'react-native-elements';
+import { Ionicons } from '@expo/vector-icons';
+import { useTranslation } from 'react-i18next';
 export default function Ingredients() {
   const [ingredients, setIngredients] = useState([]);
+  const { t } = useTranslation();
   return (
     <View>
-      <Text h4>Ingredientes</Text>
-      <TouchableOpacity></TouchableOpacity>
+      <Text h4>{t('addRecipeScreen.ingredients')}</Text>
+      <View style={{ flex: 1, flexDirection: 'row', justifyContent: 'center' }}>
+        <TouchableOpacity
+          style={{ flexDirection: 'row', alignItems: 'center' }}
+        >
+          <Ionicons name="add" size={20} />
+          <Text style={{ fontWeight: '500', fontSize: 15, marginLeft: 10 }}>
+            {t('addRecipeScreen.ingredient')}
+          </Text>
+        </TouchableOpacity>
+      </View>
     </View>
   );
 }
