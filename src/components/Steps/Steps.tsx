c84592c0a13b3420e6507c3cf674e35fd1bd3241
Jon Zubizarreta <jonzubi97@gmail.com>
2023-05-02 23:44:02 +0200

    Inicializando Steps

--- src/components/Steps/Steps.tsx
+++ src/components/Steps/Steps.tsx
@@ -1,8 +1,17 @@
+import { ImagePickerAsset } from 'expo-image-picker';
+import { useState } from 'react';
 import { useTranslation } from 'react-i18next';
 import { View } from 'react-native';
 import { Text } from 'react-native-elements';
+
+interface Step {
+  description: string;
+  image?: ImagePickerAsset;
+}
 export default function Steps() {
   const { t } = useTranslation();
+  const [steps, setSteps] = useState<Step[]>([{ description: '' }]);
+
   return (
     <View style={{ flex: 1, flexDirection: 'column' }}>
       <Text h4>{t('addRecipeScreen.steps')}</Text>
