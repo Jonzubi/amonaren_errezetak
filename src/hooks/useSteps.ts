6c19756b26e74974a363e1754afc17dd54fdd166
Jon Zubizarreta <jonzubi97@gmail.com>
2023-05-09 18:22:35 +0200

    Create useSteps.ts

---
+++ src/hooks/useSteps.ts
@@ -0,0 +1,25 @@
+import { useState } from 'react';
+import { Step } from '../components/Steps/Steps';
+
+export function useSteps() {
+  const [steps, setSteps] = useState<Step[]>([{ description: '' }]);
+
+  const addStep = () => {
+    const newSteps: Step[] = [...steps, { description: '' }];
+    setSteps(newSteps);
+  };
+  const deleteStep = (index: number) => {
+    const newSteps = steps.filter((step, i) => i !== index);
+    setSteps(newSteps);
+  };
+
+  const editStep = (index: number, newStep: Step) => {
+    const newSteps: Step[] = steps.map((step, i) => {
+      if (index !== i) return step;
+      return newStep;
+    });
+    setSteps(newSteps);
+  };
+
+  return { steps, addStep, deleteStep, editStep };
+}
