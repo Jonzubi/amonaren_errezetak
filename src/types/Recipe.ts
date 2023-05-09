1993f05d9515482e0239a0ab760b45399cf91764
Jon Zubizarreta <jonzubi97@gmail.com>
2023-05-09 17:43:33 +0200

    Actualizo interface de Receta

--- src/types/Recipe.ts
+++ src/types/Recipe.ts
@@ -1,4 +1,10 @@
+import { ImagePickerAsset } from 'expo-image-picker';
+import { Step } from '../components/Steps/Steps';
+
 export interface Recipe {
   title: string;
   description: string;
+  image?: string;
+  ingredients: string[];
+  steps: Step[];
 }
