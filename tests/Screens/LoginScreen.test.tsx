ed8ac89ac30092643d0c23a48441a5bc0d9e1399
Jon Zubizarreta <jonzubi97@gmail.com>
2023-05-11 23:09:42 +0200

    Meto provider y nav en el test

--- tests/Screens/LoginScreen.test.tsx
+++ tests/Screens/LoginScreen.test.tsx
@@ -1,6 +1,15 @@
+import { NavigationContainer } from '@react-navigation/native';
 import { render, screen, fireEvent } from '@testing-library/react-native';
+import { Provider } from 'react-redux';
+import { store } from '../../src/redux/store';
 import LoginScreen from '../../src/screens/LoginScreen/LoginScreen';
 
 test('renders correctly', () => {
-  render(<LoginScreen />);
+  render(
+    <Provider store={store}>
+      <NavigationContainer>
+        <LoginScreen />
+      </NavigationContainer>
+    </Provider>,
+  );
 });
