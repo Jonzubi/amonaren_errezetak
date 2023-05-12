2f83bb5f72529a1d03d913e632c23d1d8d7a4baf
Jon Zubizarreta <jonzubi97@gmail.com>
2023-05-12 17:19:45 +0200

    Configuro el jest config

--- jest.config.js
+++ jest.config.js
@@ -3,5 +3,10 @@ module.exports = {
   transformIgnorePatterns: [
     'node_modules/(?!((jest-)?react-native|@react-native(-community)?)|expo(nent)?|@expo(nent)?/.*|@expo-google-fonts/.*|react-navigation|@react-navigation/.*|@unimodules/.*|unimodules|sentry-expo|native-base|react-native-svg|@rneui)',
   ],
-  setupFilesAfterEnv: ['./jest.setup.ts'],
+  setupFilesAfterEnv: ['@testing-library/react-native', './jest.setup.ts'],
+  moduleNameMapper: {
+    '^expo$': 'expo/build/Expo.fx',
+    '^expo/(.*)$': 'expo/build/$1',
+    '^@expo/(.*)$': '<rootDir>/node_modules/@expo/$1',
+  },
 };
