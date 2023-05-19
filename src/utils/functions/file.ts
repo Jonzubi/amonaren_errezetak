04098d5b1a2fff58b12a537134e4a94fd3f4b727
Jon Zubizarreta <jonzubi97@gmail.com>
2023-05-19 16:34:10 +0200

    convertToBase64 creado

--- src/utils/functions/file.ts
+++ src/utils/functions/file.ts
@@ -1,4 +1,5 @@
 import { Platform } from 'react-native';
+import * as FileSystem from 'expo-file-system';
 
 const getFileUri = (uri?: string): string => {
   if (uri === null || uri === undefined) return '';
@@ -6,4 +7,16 @@ const getFileUri = (uri?: string): string => {
   return Platform.OS === 'ios' ? uri.replace('file://', '') : uri;
 };
 
-export { getFileUri };
+const convertToBase64 = async (imageUri: string): Promise<string> => {
+  try {
+    const base64 = await FileSystem.readAsStringAsync(imageUri, {
+      encoding: FileSystem.EncodingType.Base64,
+    });
+    return base64;
+  } catch (error) {
+    console.error('Error al convertir la imagen a base64:', error);
+    return '';
+  }
+};
+
+export { getFileUri, convertToBase64 };
