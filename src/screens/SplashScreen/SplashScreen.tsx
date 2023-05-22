905b07ec03de101ff28893126ef7788ece8e4ad1
Jon Zubizarreta <jonzubi97@gmail.com>
2023-05-22 19:30:05 +0200

    El splash resetea la navegacion y va a auth

--- src/screens/SplashScreen/SplashScreen.tsx
+++ src/screens/SplashScreen/SplashScreen.tsx
@@ -2,10 +2,22 @@ import { View } from 'react-native';
 import styles from './SplashScreen.android.styles';
 import { useEffect } from 'react';
 import Logo from '../../components/Logo/Logo';
+import { CommonActions } from '@react-navigation/native';
 
 export default function SplashScreen({ navigation }: { navigation: any }) {
   useEffect(() => {
-    performTimeConsumingTask().then(() => navigation.navigate('Login', {}));
+    performTimeConsumingTask().then(() => {
+      navigation.dispatch(
+        CommonActions.reset({
+          index: 1,
+          routes: [
+            {
+              name: 'Auth',
+            },
+          ],
+        }),
+      );
+    });
   }, []);
 
   const performTimeConsumingTask = async () =>
