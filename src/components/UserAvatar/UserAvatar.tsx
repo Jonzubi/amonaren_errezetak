c7fce116a1da22a79f854c8379392fc8249fa00a
Jon Zubizarreta <jonzubi97@gmail.com>
2023-05-22 20:38:07 +0200

    Como titulo pongo el nombre de usuario

--- src/components/UserAvatar/UserAvatar.tsx
+++ src/components/UserAvatar/UserAvatar.tsx
@@ -5,9 +5,11 @@ import { useSelector } from 'react-redux';
 
 export default function UserAvatar() {
   const imageUrl = useSelector((state: RootState) => state.user.imageUrl);
+  const username = useSelector((state: RootState) => state.user.username);
 
   return (
     <Avatar
+      title={username}
       size={25}
       rounded
       source={{
