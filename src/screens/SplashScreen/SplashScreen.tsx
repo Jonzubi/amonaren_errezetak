5f07ab3a036052793b3bbe4a226691a558c9108b
Jon Zubizarreta <jonzubi97@gmail.com>
2023-05-23 22:43:53 +0200

    SplashScreen intenta recoger el access token del async storage

--- src/screens/SplashScreen/SplashScreen.tsx
+++ src/screens/SplashScreen/SplashScreen.tsx
@@ -3,20 +3,28 @@ import styles from './SplashScreen.android.styles';
 import { useEffect } from 'react';
 import Logo from '../../components/Logo/Logo';
 import { CommonActions } from '@react-navigation/native';
+import { getProfile } from '../../api/user/user';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 export default function SplashScreen({ navigation }: { navigation: any }) {
   useEffect(() => {
-    performTimeConsumingTask().then(() => {
-      navigation.dispatch(
-        CommonActions.reset({
-          index: 1,
-          routes: [
-            {
-              name: 'Auth',
-            },
-          ],
-        }),
-      );
+    performTimeConsumingTask().then(async () => {
+      try {
+        const accessToken = await AsyncStorage.getItem('access_token');
+        const data = await getProfile(accessToken || '');
+        console.log(data);
+      } catch (error) {
+        navigation.dispatch(
+          CommonActions.reset({
+            index: 1,
+            routes: [
+              {
+                name: 'Auth',
+              },
+            ],
+          }),
+        );
+      }
     });
   }, []);
 
