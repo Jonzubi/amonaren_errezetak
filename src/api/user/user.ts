77abe72800d34bfdc7363c80ef938a2733c0195e
Jon Zubizarreta <jonzubi97@gmail.com>
2023-05-23 22:39:49 +0200

    Preparo llamada a getProfile

--- src/api/user/user.ts
+++ src/api/user/user.ts
@@ -6,3 +6,9 @@ export const createUser = (user: User) => axios.post(`${API_URL}/user`, user);
 export const login = (user: User) => axios.post(`${API_URL}/auth/login`, user);
 export const loginGoogle = ({ token }: { token: string }) =>
   axios.post(`${API_URL}/auth/googleLogin`, { token });
+export const getProfile = (token: string) =>
+  axios.get(`${API_URL}/auth/profile`, {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  });
