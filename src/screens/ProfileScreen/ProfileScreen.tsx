556d2f96029462111a692224a9746825304f9f86
Jon Zubizarreta <jonzubi97@gmail.com>
2023-05-25 22:08:18 +0200

    Creo ProfileScreen

---
+++ src/screens/ProfileScreen/ProfileScreen.tsx
@@ -0,0 +1,10 @@
+import { View, Text } from 'react-native';
+import styles from './ProfileScreen.android.styles';
+
+export default function ProfileScreen() {
+  return (
+    <View>
+      <Text>Profile</Text>
+    </View>
+  );
+}
