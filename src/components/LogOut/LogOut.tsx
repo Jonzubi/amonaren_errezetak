2fea51735dc0d9628610644c68cf013bb2897b21
Jon Zubizarreta <jonzubi97@gmail.com>
2023-05-25 22:01:05 +0200

    Logout funcional

--- src/components/LogOut/LogOut.tsx
+++ src/components/LogOut/LogOut.tsx
@@ -1,13 +1,25 @@
-import { TouchableOpacity, View } from 'react-native';
+import { TouchableOpacity } from 'react-native';
 import { AntDesign } from '@expo/vector-icons';
 import { useTranslation } from 'react-i18next';
 import { Text } from 'react-native-elements';
 import styles from './Logout.android.styles';
+import { useDispatch } from 'react-redux';
+import { resetUserData } from '../../redux/user/userSlice';
+import { useNavigation } from '@react-navigation/native';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 export default function LogOut() {
   const { t } = useTranslation();
+  const dispatch = useDispatch();
+  const navigation = useNavigation();
+
+  const handlePress = () => {
+    dispatch(resetUserData());
+    AsyncStorage.setItem('access_token', '');
+    navigation.navigate('Auth');
+  };
   return (
-    <TouchableOpacity style={styles.container}>
+    <TouchableOpacity style={styles.container} onPress={handlePress}>
       <AntDesign name="logout" />
       <Text>{t('logOut.logout')}</Text>
     </TouchableOpacity>
