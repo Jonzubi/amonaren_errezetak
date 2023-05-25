52e10882a81952f7ed5e6af517f74e17404c8e9c
Jon Zubizarreta <jonzubi97@gmail.com>
2023-05-25 22:49:46 +0200

    Maquetando ProfileScreen, falta boton

--- src/screens/ProfileScreen/ProfileScreen.android.styles.ts
+++ src/screens/ProfileScreen/ProfileScreen.android.styles.ts
@@ -1,2 +1,17 @@
 import { StyleSheet } from 'react-native';
-export default StyleSheet.create({});
+import colors from '../../constants/colors';
+export default StyleSheet.create({
+  container: {
+    padding: 15,
+  },
+  userMainContainer: {
+    flexDirection: 'row',
+    gap: 10,
+    alignItems: 'flex-end',
+  },
+  usernameText: {
+    color: colors.BLACK,
+    fontStyle: 'italic',
+  },
+  formContainer: {},
+});
--- src/screens/ProfileScreen/ProfileScreen.tsx
+++ src/screens/ProfileScreen/ProfileScreen.tsx
@@ -1,11 +1,33 @@
-import { Text } from 'react-native';
+import { View } from 'react-native';
 import styles from './ProfileScreen.android.styles';
 import { SafeAreaView } from 'react-native-safe-area-context';
+import UserAvatar from '../../components/UserAvatar/UserAvatar';
+import { useSelector } from 'react-redux';
+import { RootState } from '../../redux/store';
+import { Divider, Text } from 'react-native-elements';
+import { Input } from '@rneui/themed';
+import { useTranslation } from 'react-i18next';
+import { useState } from 'react';
 
 export default function ProfileScreen() {
+  const username = useSelector((state: RootState) => state.user.username);
+  const { t } = useTranslation();
+
+  const [newUsername, setNewUsername] = useState('');
+
   return (
-    <SafeAreaView>
-      <Text>Profile</Text>
+    <SafeAreaView style={styles.container}>
+      <View style={styles.userMainContainer}>
+        <UserAvatar size={100} />
+        <Text style={styles.usernameText}>{username}</Text>
+      </View>
+      <Divider style={{ marginVertical: 15 }} />
+      <View>
+        <Input
+          placeholder={t('forms.username_placeholder')}
+          onChangeText={(value) => setNewUsername(value)}
+        />
+      </View>
     </SafeAreaView>
   );
 }
