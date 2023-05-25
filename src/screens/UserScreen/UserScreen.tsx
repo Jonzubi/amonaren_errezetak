5f8a9a4d024b2a38f19c9d85588827d3c5cf8f00
Jon Zubizarreta <jonzubi97@gmail.com>
2023-05-25 22:19:45 +0200

    Desde user se puede navegar a profile

--- src/screens/UserScreen/UserScreen.tsx
+++ src/screens/UserScreen/UserScreen.tsx
@@ -1,4 +1,4 @@
-import { Text, View } from 'react-native';
+import { Text, View, TouchableOpacity } from 'react-native';
 import styles from './UserScreen.android.styles';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import UserAvatar from '../../components/UserAvatar/UserAvatar';
@@ -6,17 +6,26 @@ import LogOut from '../../components/LogOut/LogOut';
 import { Divider } from '@rneui/base';
 import { useSelector } from 'react-redux';
 import { RootState } from '../../redux/store';
+import { useNavigation } from '@react-navigation/native';
 
 export default function UserScreen() {
+  const navigation = useNavigation();
   const username = useSelector((state: RootState) => state.user.username);
+
+  const handleGoProfile = () => {
+    navigation.navigate('Profile');
+  };
 
   return (
     <SafeAreaView>
       <View style={styles.header}>
-        <View style={styles.headerUserAvatarContainer}>
+        <TouchableOpacity
+          style={styles.headerUserAvatarContainer}
+          onPress={handleGoProfile}
+        >
           <UserAvatar size={40} />
           <Text style={styles.usernameText}>{username}</Text>
-        </View>
+        </TouchableOpacity>
         <LogOut />
       </View>
       <Divider />
