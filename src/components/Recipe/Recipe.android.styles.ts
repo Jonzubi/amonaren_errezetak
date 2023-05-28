5958ea321f5a8df58d2917a7aa503a9835a3d085
Jon Zubizarreta <jonzubi97@gmail.com>
2023-05-28 13:06:51 +0200

    Recetas estolo mejorado

--- src/components/Recipe/Recipe.android.styles.ts
+++ src/components/Recipe/Recipe.android.styles.ts
@@ -1,7 +1,28 @@
 import { StyleSheet } from 'react-native';
+import colors from '../../constants/colors';
 
 export default StyleSheet.create({
   container: {
     flex: 1,
   },
+  cardFooter: {
+    marginTop: 15,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+  },
+  cardFooterUser: {
+    flexDirection: 'row',
+    gap: 5,
+    alignItems: 'center',
+  },
+  cardFooterRating: {
+    gap: 15,
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  cardFooterRate: {
+    flexDirection: 'row',
+    gap: 5,
+  },
+  cardFooterUsernameText: {},
 });
--- src/components/Recipe/Recipe.tsx
+++ src/components/Recipe/Recipe.tsx
@@ -1,21 +1,48 @@
+import { View } from 'react-native';
 import { Card, Text } from '@rneui/themed';
 import styles from './Recipe.android.styles';
 import { API_URL } from '../../constants/constants';
+import UserAvatar from '../UserAvatar/UserAvatar';
+import { AntDesign } from '@expo/vector-icons';
+import colors from '../../constants/colors';
 
 interface Props {
   title: string;
   description: string;
   image?: string;
+  createdBy: createdByProp;
 }
 
-export default function Recipe({ title, description, image }: Props) {
+interface createdByProp {
+  username: string;
+  imageUrl?: string;
+}
+
+export default function Recipe({ title, image, createdBy }: Props) {
   return (
     <Card containerStyle={styles.container}>
       <Card.Title>
         <Text h4>{title}</Text>
       </Card.Title>
       <Card.Image source={{ uri: `${API_URL}/images/${image}` }} />
-      <Card.Divider />
+      <View style={styles.cardFooter}>
+        <View style={styles.cardFooterUser}>
+          <UserAvatar hardCodedImageUrl={createdBy?.imageUrl} hardCodeUrl />
+          <Text style={styles.cardFooterUsernameText}>
+            {createdBy?.username}
+          </Text>
+        </View>
+        <View style={styles.cardFooterRating}>
+          <View style={styles.cardFooterRate}>
+            <AntDesign name="hearto" size={20} color={colors.RED} />
+            <Text>25</Text>
+          </View>
+          <View style={styles.cardFooterRate}>
+            <AntDesign name="staro" size={20} color={colors.BLACK} />
+            <Text>10</Text>
+          </View>
+        </View>
+      </View>
     </Card>
   );
 }
