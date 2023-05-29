ee912e7a25f77140655bbbcb52128070be746ef3
Jon Zubizarreta <jonzubi97@gmail.com>
2023-05-29 22:06:07 +0200

    My recipes maquetado

--- src/screens/MyRecipes/MyRecipes.android.styles.ts
+++ src/screens/MyRecipes/MyRecipes.android.styles.ts
@@ -1,2 +1,41 @@
 import { StyleSheet } from 'react-native';
-export default StyleSheet.create({});
+import colors from '../../constants/colors';
+export default StyleSheet.create({
+  container: {},
+  myRecipeContainer: {
+    flexDirection: 'row',
+    gap: 20,
+  },
+  myRecipeImage: {
+    height: 75,
+    width: 75,
+  },
+  myRecipeInfoContainer: {
+    flex: 1,
+    flexDirection: 'column',
+    justifyContent: 'space-between',
+  },
+  myRecipeTitle: {
+    fontWeight: 'bold',
+    fontSize: 15,
+  },
+  myRecipeSubInfoContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+  },
+  myRecipesRatingContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    gap: 10,
+  },
+  myRecipesRatingSubContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    gap: 5,
+  },
+  myRecipesDate: {
+    color: colors.DARKGREY,
+    fontStyle: 'italic',
+  },
+});
--- src/screens/MyRecipes/MyRecipes.tsx
+++ src/screens/MyRecipes/MyRecipes.tsx
@@ -10,23 +10,53 @@ import { useRecipes } from '../../hooks/useRecipes';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import { Recipe as TRecipe } from '../../types/Recipe';
 import { useState } from 'react';
+import styles from './MyRecipes.android.styles';
+import { Image } from 'react-native-elements';
+import { getImageUrlWithName } from '../../utils/functions/image';
+import { Card } from '@rneui/themed';
+import { AntDesign } from '@expo/vector-icons';
+import colors from '../../constants/colors';
+
 export default function MyRecipes() {
   const { recipes, loading, refreshRecipes } = useRecipes(true);
   const [refreshing, setRefreshing] = useState(false);
 
-  const handleRenderItem: ListRenderItem<TRecipe> = ({ item }) => <View></View>;
+  const handleRenderItem: ListRenderItem<TRecipe> = ({ item }) => (
+    <Card wrapperStyle={styles.myRecipeContainer}>
+      <Image
+        source={{ uri: getImageUrlWithName(item.image) }}
+        style={styles.myRecipeImage}
+      />
+      <View style={styles.myRecipeInfoContainer}>
+        <Text style={styles.myRecipeTitle}>{item.title}</Text>
+        <View style={styles.myRecipeSubInfoContainer}>
+          <View style={styles.myRecipesRatingContainer}>
+            <View style={styles.myRecipesRatingSubContainer}>
+              <AntDesign name="hearto" size={20} color={colors.RED} />
+              <Text>25</Text>
+            </View>
+            <View style={styles.myRecipesRatingSubContainer}>
+              <AntDesign name="staro" size={20} color={colors.BLACK} />
+              <Text>10</Text>
+            </View>
+          </View>
+          <Text style={styles.myRecipesDate}>Hace 5 días</Text>
+        </View>
+      </View>
+    </Card>
+  );
 
   return (
-    <ScrollView
-      refreshControl={
-        <RefreshControl refreshing={refreshing} onRefresh={refreshRecipes} />
-      }
-    >
+    <SafeAreaView>
       <FlatList
+        contentContainerStyle={styles.container}
+        refreshControl={
+          <RefreshControl refreshing={refreshing} onRefresh={refreshRecipes} />
+        }
         data={recipes}
         renderItem={handleRenderItem}
         keyExtractor={(item, index) => index.toString()}
       />
-    </ScrollView>
+    </SafeAreaView>
   );
 }
