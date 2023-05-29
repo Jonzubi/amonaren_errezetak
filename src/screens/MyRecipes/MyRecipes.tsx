4d36a6747b4cc17772f486c4e68eb8a9fd4c8dd8
Jon Zubizarreta <jonzubi97@gmail.com>
2023-05-29 21:35:19 +0200

    MyRecipes renderizando lista

--- src/screens/MyRecipes/MyRecipes.tsx
+++ src/screens/MyRecipes/MyRecipes.tsx
@@ -1,8 +1,32 @@
-import { View, Text } from 'react-native';
+import {
+  View,
+  Text,
+  ScrollView,
+  RefreshControl,
+  FlatList,
+  ListRenderItem,
+} from 'react-native';
+import { useRecipes } from '../../hooks/useRecipes';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import { Recipe as TRecipe } from '../../types/Recipe';
+import { useState } from 'react';
 export default function MyRecipes() {
+  const { recipes, loading, refreshRecipes } = useRecipes(true);
+  const [refreshing, setRefreshing] = useState(false);
+
+  const handleRenderItem: ListRenderItem<TRecipe> = ({ item }) => <View></View>;
+
   return (
-    <View>
-      <Text>My Recipes</Text>
-    </View>
+    <ScrollView
+      refreshControl={
+        <RefreshControl refreshing={refreshing} onRefresh={refreshRecipes} />
+      }
+    >
+      <FlatList
+        data={recipes}
+        renderItem={handleRenderItem}
+        keyExtractor={(item, index) => index.toString()}
+      />
+    </ScrollView>
   );
 }
