00801f213b7e152f405005e563c03e29e06af2aa
Jon Zubizarreta <jonzubi97@gmail.com>
2023-06-07 22:20:01 +0200

    fix: options es obligatorio

--- src/api/recipe/recipe.ts
+++ src/api/recipe/recipe.ts
@@ -3,23 +3,23 @@ import { API_URL } from '../../constants/constants';
 import { Recipe } from '../../types/Recipe';
 import axios from '../axios';
 
-export const createRecipe = (recipe: Recipe, options?: AxiosRequestConfig) =>
+export const createRecipe = (recipe: Recipe, options: AxiosRequestConfig) =>
   axios.post(`${API_URL}/recipe`, recipe, options);
 
-export const likeRecipe = (recipeId: string, options?: AxiosRequestConfig) =>
+export const likeRecipe = (recipeId: string, options: AxiosRequestConfig) =>
   axios.post(`${API_URL}/recipe/like`, { recipeId }, options);
 
-export const unlikeRecipe = (recipeId: string, options?: AxiosRequestConfig) =>
+export const unlikeRecipe = (recipeId: string, options: AxiosRequestConfig) =>
   axios.post(`${API_URL}/recipe/unlike`, { recipeId }, options);
 
-export const favRecipe = (recipeId: string, options?: AxiosRequestConfig) =>
+export const favRecipe = (recipeId: string, options: AxiosRequestConfig) =>
   axios.post(`${API_URL}/recipe/fav`, { recipeId }, options);
 
-export const unfavRecipe = (recipeId: string, options?: AxiosRequestConfig) =>
+export const unfavRecipe = (recipeId: string, options: AxiosRequestConfig) =>
   axios.post(`${API_URL}/recipe/unfav`, { recipeId }, options);
 
-export const getRecipes = (options?: AxiosRequestConfig) =>
+export const getRecipes = (options: AxiosRequestConfig) =>
   axios.get(`${API_URL}/recipe`, options);
 
-export const getMyRecipes = (options?: AxiosRequestConfig) =>
+export const getMyRecipes = (options: AxiosRequestConfig) =>
   axios.get(`${API_URL}/recipe/myrecipes`, options);
