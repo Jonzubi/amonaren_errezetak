bd1a167e10b03870703ae1984c67c71aa74b57e7
Jon Zubizarreta <jonzubi97@gmail.com>
2023-06-07 22:20:59 +0200

    feat: RateRecipeIcon
    Cuando se clicka cambia el icono y suma al valor que tiene almacenado. Si se vuelve a clickar se resta

--- src/components/RateRecipeIcon/RateRecipeIcon.tsx
+++ src/components/RateRecipeIcon/RateRecipeIcon.tsx
@@ -1,7 +1,16 @@
+import { useState, useEffect, useRef } from 'react';
 import { ViewStyle, Text, TouchableOpacity } from 'react-native';
 import { AntDesign } from '@expo/vector-icons';
 import colors from '../../constants/colors';
-import { likeRecipe } from '../../api/recipe/recipe';
+import {
+  likeRecipe,
+  unlikeRecipe,
+  favRecipe,
+  unfavRecipe,
+} from '../../api/recipe/recipe';
+import { getHeaderWithAccessToken } from '../../utils/functions/axiosOptions';
+import { useSelector } from 'react-redux';
+import { RootState } from '../../redux/store';
 
 interface RateRecipeIconProps {
   containerStyle?: ViewStyle;
@@ -17,20 +26,44 @@ export default function RateRecipeIcon({
   rateCount,
   recipeId,
 }: RateRecipeIconProps) {
+  const token = useSelector((state: RootState) => state.user.access_token);
+  const [auxIsRated, setAuxIsRated] = useState(isRated);
+  const [auxRateCount, setAuxRateCount] = useState(rateCount);
+  const isInitialMount = useRef(true);
+
   const getIconName = () => {
-    if (type === 'Like') return isRated ? 'heart' : 'hearto';
-    return isRated ? 'star' : 'staro';
+    if (type === 'Like') return auxIsRated ? 'heart' : 'hearto';
+    return auxIsRated ? 'star' : 'staro';
+  };
+  const getAction = () => {
+    if (type === 'Like') return auxIsRated ? unlikeRecipe : likeRecipe;
+    return auxIsRated ? unfavRecipe : favRecipe;
   };
   const getColor = () => (type === 'Like' ? colors.RED : colors.BLACK);
 
   const handlePress = () => {
-    likeRecipe(recipeId);
+    const action = getAction();
+
+    try {
+      action(recipeId, getHeaderWithAccessToken(token));
+      setAuxIsRated(!auxIsRated);
+    } catch (error) {
+      console.log({ error });
+    }
   };
+
+  useEffect(() => {
+    if (isInitialMount.current) {
+      isInitialMount.current = false;
+      return;
+    }
+    setAuxRateCount(auxIsRated ? auxRateCount + 1 : auxRateCount - 1);
+  }, [auxIsRated]);
 
   return (
     <TouchableOpacity onPress={handlePress} style={containerStyle}>
       <AntDesign name={getIconName()} size={20} color={getColor()} />
-      <Text>{rateCount}</Text>
+      <Text>{auxRateCount}</Text>
     </TouchableOpacity>
   );
 }
