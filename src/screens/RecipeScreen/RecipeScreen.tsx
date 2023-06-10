8e0cc1f232ddbcbf5c24a8f0a4f985b550ff905f
Jon Zubizarreta <jonzubi97@gmail.com>
2023-06-10 14:51:33 +0200

    feat: Creo ficheros para ver recetas

---
+++ src/screens/RecipeScreen/RecipeScreen.tsx
@@ -0,0 +1,6 @@
+import { SafeAreaView } from 'react-native';
+import styles from './RecipeScreen.android.styles';
+
+export default function RecipeScreen() {
+  return <SafeAreaView></SafeAreaView>;
+}
