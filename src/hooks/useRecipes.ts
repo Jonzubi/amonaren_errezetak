c55dcd4e984697232ca7f6bd12b7bd45289e4c75
Jon Zubizarreta <jonzubi97@gmail.com>
2023-06-12 13:54:54 +0200

    feat: UseRecipes puede obtener con ID

--- src/hooks/useRecipes.ts
+++ src/hooks/useRecipes.ts
@@ -4,25 +4,34 @@ import { useSelector } from 'react-redux';
 import { RootState } from '../redux/store';
 import { Recipe } from '../types/Recipe';
 import { getHeaderWithAccessToken } from '../utils/functions/axiosOptions';
-import { getFavRecipes, getMyRecipes, getRecipes } from '../api/recipe/recipe';
+import {
+  getFavRecipes,
+  getMyRecipes,
+  getRecipeById,
+  getRecipes,
+} from '../api/recipe/recipe';
 
 export enum UseRecipesType {
   ALL,
   MINE,
   FAVS,
+  BYID,
 }
 
-export function useRecipes(type: UseRecipesType) {
+export function useRecipes(type: UseRecipesType, recipeId?: string): any {
+  if (type === UseRecipesType.BYID && recipeId === undefined) {
+    throw new Error('recipeId is required when type is BYID');
+  }
   const [recipes, setRecipes] = useState<Recipe[]>([]);
   const [loading, setLoading] = useState(true);
   const token = useSelector((state: RootState) => state.user.access_token);
 
-  const typeToFetch = [getRecipes, getMyRecipes, getFavRecipes];
+  const typeToFetch = [getRecipes, getMyRecipes, getFavRecipes, getRecipeById];
   const fetchUrl = typeToFetch[type];
 
   const fetchData = () => {
     setRecipes([]);
-    fetchUrl(getHeaderWithAccessToken(token)).then((data) => {
+    fetchUrl(getHeaderWithAccessToken(token), recipeId).then((data) => {
       setRecipes(
         data.data.map((d: any) => ({
           recipeId: d._id,
