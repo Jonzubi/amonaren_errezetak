df7a4c4f09c3af1a60b055b96e97004e24c8a132
Jon Zubizarreta <jonzubi97@gmail.com>
2023-06-12 23:07:01 +0200

    Diseñando recipeDetails, hasta favButton

--- src/components/RecipeDetails/RecipeDetails.android.styles.ts
+++ src/components/RecipeDetails/RecipeDetails.android.styles.ts
@@ -1,8 +1,22 @@
 import { StyleSheet, Dimensions } from 'react-native';
+import colors from '../../constants/colors';
 const { width, height } = Dimensions.get('window');
 
 export default StyleSheet.create({
-  container: { flex: 1 },
+  container: {},
+  detailContainer: {
+    padding: 15,
+  },
+  detailUserContainer: {
+    flexDirection: 'row',
+    gap: 15,
+  },
+  detailUserNameContainer: {
+    flexDirection: 'column',
+  },
+  detailUserNameText: {
+    color: colors.DARKGREY,
+  },
   recipeImage: {
     width: width,
     height: height,
@@ -11,5 +25,26 @@ export default StyleSheet.create({
   },
   title: {
     fontWeight: 'bold',
+    fontSize: 25,
+    marginVertical: 15,
+  },
+  description: {
+    marginVertical: 15,
+  },
+  favouriteButton: {
+    marginVertical: 15,
+    border: 1,
+    borderWidth: 1,
+    borderColor: colors.DARKGREY,
+    borderRadius: 10,
+    backgroundColor: colors.GREY,
+    height: 35,
+    justifyContent: 'center',
+    alignItems: 'center',
+    flexDirection: 'row',
+    gap: 10,
+  },
+  favouriteText: {
+    fontSize: 15,
   },
 });
--- src/components/RecipeDetails/RecipeDetails.tsx
+++ src/components/RecipeDetails/RecipeDetails.tsx
@@ -1,20 +1,46 @@
-import { View, Image, Text } from 'react-native';
+import { ScrollView, Image, View, TouchableOpacity } from 'react-native';
 import styles from './RecipeDetails.android.styles';
 import { getImageUrlWithName } from '../../utils/functions/image';
+import { Text } from 'react-native-elements';
+import UserAvatar from '../UserAvatar/UserAvatar';
 import { useTranslation } from 'react-i18next';
+import { AntDesign } from '@expo/vector-icons';
 
 interface RecipeDetailsProps {
   recipe: any;
 }
 export function RecipeDetails({ recipe }: RecipeDetailsProps) {
   console.log({ recipe });
+  const { t } = useTranslation();
   return (
-    <View style={styles.container}>
+    <ScrollView style={styles.container}>
       <Image
         style={styles.recipeImage}
         source={{ uri: getImageUrlWithName(recipe.image) }}
       />
-      <Text style={styles.title}>{recipe.title}</Text>
-    </View>
+      <View style={styles.detailContainer}>
+        <Text style={styles.title}>{recipe.title}</Text>
+        <View style={styles.detailUserContainer}>
+          <UserAvatar
+            size={50}
+            hardCodeUrl
+            hardCodedImageUrl={recipe.createdBy.imageUrl}
+          />
+          <View style={styles.detailUserNameContainer}>
+            <Text>{recipe.createdBy.nameSurname}</Text>
+            <Text style={styles.detailUserNameText}>
+              {recipe.createdBy.username}
+            </Text>
+          </View>
+        </View>
+        <Text style={styles.description}>{recipe.description}</Text>
+        <TouchableOpacity style={styles.favouriteButton}>
+          <AntDesign name="star" size={20} />
+          <Text style={styles.favouriteText}>
+            {t('recipeDetail.saveFavourite')}
+          </Text>
+        </TouchableOpacity>
+      </View>
+    </ScrollView>
   );
 }
