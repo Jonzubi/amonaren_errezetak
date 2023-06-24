952332cb7f18b437cce95d614fce9c8fa63f28b5
Jon Zubizarreta <jonzubi97@gmail.com>
2023-06-24 12:35:32 +0200

    fix: Google SignIn funciona en Expo

--- src/components/SignGoogle/SignGoogle.tsx
+++ src/components/SignGoogle/SignGoogle.tsx
@@ -26,9 +26,8 @@ export default function SignGoogle() {
   const [token, setToken] = useState('');
 
   const [request, response, promptAsync] = Google.useAuthRequest({
+    clientId: GOOGLE_WEB_CLIENT_ID,
     androidClientId: GOOGLE_ANDROID_CLIENT_ID,
-    webClientId: GOOGLE_WEB_CLIENT_ID,
-    expoClientId: GOOGLE_EXPO_CLIENT_ID,
     iosClientId: GOOGLE_ANDROID_CLIENT_ID,
   });
 
