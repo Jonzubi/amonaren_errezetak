240bec47ecac64d8e15257fbbbba5b67351d3af8
Jon Zubizarreta <jonzubi97@gmail.com>
2023-07-20 23:46:12 +0200

    feat: Redux fixes URL
    Esto hay que refactorizar en la API

--- src/redux/user/userSlice.ts
+++ src/redux/user/userSlice.ts
@@ -1,4 +1,5 @@
 import { PayloadAction, createSlice } from '@reduxjs/toolkit';
+import { getImageUrlWithName } from '../../utils/functions/image';
 
 export interface UserState {
   email: string;
@@ -6,6 +7,12 @@ export interface UserState {
   username: string;
   access_token: string;
   imageUrl: string;
+}
+
+export interface ProfileData {
+  nameSurname: string;
+  username: string;
+  imageUrl?: string;
 }
 
 const initialState: UserState = {
@@ -24,15 +31,33 @@ export const userSlice = createSlice({
       state.access_token = action.payload;
     },
     setUserData: (state: UserState, action: PayloadAction<UserState>) => {
+      const auxAction = { ...action };
+      if (
+        auxAction.payload.imageUrl !== undefined &&
+        auxAction.payload.imageUrl !== '' &&
+        !auxAction.payload.imageUrl.startsWith('http')
+      )
+        auxAction.payload.imageUrl = getImageUrlWithName(
+          auxAction.payload.imageUrl,
+        );
       return { ...state, ...action.payload };
     },
-    setUserName: (state: UserState, action: PayloadAction<string>) => {
-      state.username = action.payload;
+    setUserProfile: (state: UserState, action: PayloadAction<ProfileData>) => {
+      const auxAction = { ...action };
+      if (
+        auxAction.payload.imageUrl !== undefined &&
+        auxAction.payload.imageUrl !== '' &&
+        !auxAction.payload.imageUrl.startsWith('http')
+      )
+        auxAction.payload.imageUrl = getImageUrlWithName(
+          auxAction.payload.imageUrl,
+        );
+      return { ...state, ...action.payload };
     },
     resetUserData: () => initialState,
   },
 });
 
-export const { setAccessToken, setUserData, resetUserData, setUserName } =
+export const { setAccessToken, setUserData, resetUserData, setUserProfile } =
   userSlice.actions;
 export default userSlice.reducer;
