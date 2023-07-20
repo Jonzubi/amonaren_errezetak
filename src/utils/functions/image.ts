3de9d5b437d77794073901c8cdd7515553050eb2
Jon Zubizarreta <jonzubi97@gmail.com>
2023-07-20 23:46:44 +0200

    feat: isValidBase64 implemented

--- src/utils/functions/image.ts
+++ src/utils/functions/image.ts
@@ -3,4 +3,10 @@ import { API_URL } from '../../constants/constants';
 const getImageUrlWithName = (name: string | undefined) =>
   `${API_URL}/images/${name}`;
 
-export { getImageUrlWithName };
+const isValidBase64 = (base64: string): boolean => {
+  const base64RegExp = /^data:image\/(jpeg|jpg|png|gif);base64,/;
+
+  return base64RegExp.test(base64);
+};
+
+export { getImageUrlWithName, isValidBase64 };
