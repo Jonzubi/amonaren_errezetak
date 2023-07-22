7a3c7c15676413a28a087fe6f84ebd1cb40cbb10
Jon Zubizarreta <jonzubi97@gmail.com>
2023-07-22 16:05:44 +0200

    Revert "feat: Redux fixes URL"
    This reverts commit 240bec47ecac64d8e15257fbbbba5b67351d3af8.

--- src/redux/user/userSlice.ts
+++ src/redux/user/userSlice.ts
@@ -1,5 +1,4 @@
 import { PayloadAction, createSlice } from '@reduxjs/toolkit';
-import { getImageUrlWithName } from '../../utils/functions/image';
 
 export interface UserState {
   email: string;
@@ -7,12 +6,6 @@ export interface UserState {
   username: string;
   access_token: string;
   imageUrl: string;
-}
-
-export interface ProfileData {
-  nameSurname: string;
-  username: string;
-  imageUrl?: string;
 }
 
 const initialState: UserState = {
@@ -31,33 +24,15 @@ export const userSlice = createSlice({
       state.access_token = action.payload;
     },
     setUserData: (state: UserState, action: PayloadAction<UserState>) => {
-      const auxAction = { ...action };
-      if (
-        auxAction.payload.imageUrl !== undefined &&
-        auxAction.payload.imageUrl !== '' &&
-        !auxAction.payload.imageUrl.startsWith('http')
-      )
-        auxAction.payload.imageUrl = getImageUrlWithName(
-          auxAction.payload.imageUrl,
-        );
       return { ...state, ...action.payload };
     },
-    setUserProfile: (state: UserState, action: PayloadAction<ProfileData>) => {
-      const auxAction = { ...action };
-      if (
-        auxAction.payload.imageUrl !== undefined &&
-        auxAction.payload.imageUrl !== '' &&
-        !auxAction.payload.imageUrl.startsWith('http')
-      )
-        auxAction.payload.imageUrl = getImageUrlWithName(
-          auxAction.payload.imageUrl,
-        );
-      return { ...state, ...action.payload };
+    setUserName: (state: UserState, action: PayloadAction<string>) => {
+      state.username = action.payload;
     },
     resetUserData: () => initialState,
   },
 });
 
-export const { setAccessToken, setUserData, resetUserData, setUserProfile } =
+export const { setAccessToken, setUserData, resetUserData, setUserName } =
   userSlice.actions;
 export default userSlice.reducer;
