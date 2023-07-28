f0afb29d85514dc16afb184f2da91b47a8255562
Jon Zubizarreta <jonzubi97@gmail.com>
2023-07-28 09:28:10 +0200

    fix: EsURL regexp

--- src/utils/functions/url.ts
+++ src/utils/functions/url.ts
@@ -1,7 +1,7 @@
 export const esURL = (input: string | undefined): boolean => {
   if (input === undefined) return false;
   const urlPattern = new RegExp(
-    '^((https?|ftp)://)?' + // Protocolo
+    '^(https?|ftp)://' + // Protocolo
       '((([a-zA-Z0-9]([a-zA-Z0-9-]*[a-zA-Z0-9])?)\\.)+[a-zA-Z]{2,}|' + // Dominio
       '((\\d{1,3}\\.){3}\\d{1,3}))' + // O dirección IP
       '(\\:\\d+)?(\\/[-a-zA-Z0-9%_.~+]*)*' + // Puerto y ruta
@@ -9,6 +9,9 @@ export const esURL = (input: string | undefined): boolean => {
       '(\\#[-a-zA-Z0-9%_.~+=-]*)?$', // Fragmento
     'i',
   );
-
+  console.log({
+    name: input,
+    test: urlPattern.test(input),
+  });
   return urlPattern.test(input);
 };
