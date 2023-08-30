dde66c1d6a4d77e0a1a2019b94f87f68b688a2a2
Jon Zubizarreta <jonzubi97@gmail.com>
2023-08-30 16:47:50 +0200

    Delete [recipeId].tsx

--- src/app/main/user/recipes/[recipeId].tsx
+++
@@ -1,6 +0,0 @@
-import AddRecipeScreen from '@screens/AddRecipeScreen/AddRecipeScreen';
-import { useLocalSearchParams } from 'expo-router';
-export default function RecipeDetailsScreen() {
-  const { recipeId } = useLocalSearchParams<{ recipeId: string }>();
-  return <AddRecipeScreen recipeId={recipeId} />;
-}
